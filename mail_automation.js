5de43edf175bc10c2b7eb33baf94ba20c39f0c90
vishalned <vishalned@gmail.com>
2019-08-19 19:51:12 +0530

    email automation

--- mail_automation.js
+++ mail_automation.js
@@ -1,10 +1,21 @@
 var sent = document.getElementById('mail')
+var roll = document.getElementById('roll')
 
-
+var messages = ['Student is facing a medical issue. Urgent approval of pass is requested.',"A member of students's family is not keeping well. Urgent approval of pass is requested.","Student is going out on College Fest Related Work. Urgent approval of pass is requested.",'Student has to go outside for project related work. Urgent approval of pass is requested.']
+var mails = ['test1@example.com','test2@example.com','test3@example.com','test4@example.com']
 mail.addEventListener('click',()=>{
-	
+roll_text = roll.value;
+roll_text = roll_text.toLowerCase();
+let reg = /\b(cb|am|bl).en.u4(cse|ece|eee|che|civ)[0-9][0-9][0-9][0-9][0-9]\b/
+if(!reg.test(roll_text)){
+alert('Enter a valid roll number')
+}
+else{
     var authority = document.getElementById('drop1')
     var reason = document.getElementById('drop2')
-    var mail = "mailto:test@example.com?subject="+reason.value+"&body=Student is facing a medical issue. Urgent approval of pass is requested."
+      
+    var mail = `mailto:${mails[authority.selectedIndex]}?subject=`+reason.value+'-'+roll_text+`&body=${messages[reason.selectedIndex]}`;
     window.location.href = mail
+    
+}
 })
--- sign_up_student.js
+++ sign_up_student.js
@@ -5,7 +5,7 @@ sub.addEventListener('click',()=>{
 	roll_text = roll_text.toLowerCase();
 	let reg = /\b(cb|am|bl).en.u4(cse|ece|eee|che|civ)[0-9][0-9][0-9][0-9][0-9]\b/
 	if(reg.test(roll_text)){
-		window.location.href = 'apply_pass.html'
+		//window.location.href = 'apply_pass.html'
 	}
 	else{
         alert('enter roll number in the proper format')
